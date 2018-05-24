d39a37df7c44061b977386de841c5891967fd4c0
Caren Lierz <carenlierz@hotmail.com>
2018-05-24 09:10:15 -0500

    Adding Exercise 1.4

---
+++ week-1/Lierz_Ex1.4.js
@@ -0,0 +1,34 @@
+// start program
+
+/*
+    Expected output:
+    
+    First name
+    Last name
+    Age
+    Street
+    City
+    State 
+    Zip code 
+*/ 
+
+// variable declaration and assignment goes here ...
+var firstName = "Caren";
+var lastName = "Lierz";
+var age = "39";
+var street = "8560 S 102"
+var city = "La Vista"
+var state = "Nebraska"
+var zipCode = "68128"
+
+// output 
+console.log(firstName)
+console.log(lastName);
+console.log(age);
+console.log(street);
+console.log(city);
+console.log(state);
+console.log(zipCode);
+
+
+// end program 
