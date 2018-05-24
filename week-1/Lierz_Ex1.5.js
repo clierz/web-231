4505f3eef3913ee0a3465c7f060d16d59fc59647
Caren Lierz <carenlierz@hotmail.com>
2018-05-24 11:06:02 -0500

    Adding assignment 1.5

---
+++ week-1/Lierz_Ex1.5.js
@@ -0,0 +1,46 @@
+/*
+============================================
+; Title:  Assignment 1.5
+; Author: Albert Einstein
+; Date:   25 June 2017
+; Modified By: <Your name>
+; Description: This program demonstrates the
+;   use of JavaScript types, values, and 
+;   and variables in an application.
+;===========================================
+*/ 
+
+/*
+    The code below builds a properly formatted
+    header and must be included in all applications
+    you write. In subsequent week's we will build this
+    functionality into a function and place it in a separate file.
+*/
+
+var myFirstName = "Caren";
+var myLastName = "Lierz";
+var todaysDate = new Date().toLocaleDateString();
+var assignmentNum = "Assignment 1.5";
+
+var programHeader = "\n" + myFirstName + " " + myLastName + "\n"
+    + assignmentNum + "\nDate: " + todaysDate;
+
+console.log(programHeader);
+console.log("\n"); 
+
+// start of program - your code goes below this line
+
+var firstName = "Jane";
+var lastName = "Doe";
+var address = "8560 S 102 St La Vista NE, 68128";
+var payRate = "$10.50";
+var hireDate = "5/24/2018";
+
+console.log ("First Name:", firstName);
+console.log("Last Name:", lastName);
+console.log("Address:", address);
+console.log("Pay Rate:", payRate);
+console.log("Hire Date:", hireDate)
+
+
+// end of program 
