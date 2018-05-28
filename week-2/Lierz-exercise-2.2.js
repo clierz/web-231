15cddb76477607b044216d40749ca853046c0e0b
Caren Lierz <carenlierz@hotmail.com>
2018-05-28 17:55:03 -0500

    Adding assignment 2.2 files

---
+++ week-2/Lierz-exercise-2.2.js
@@ -0,0 +1,6 @@
+var header = require('../header.js');
+
+console.log("\nTesting the header.js file's output..."); 
+
+console.log(header.display("Caren", "Lierz", "Exercise 2.1")); 
+console.log("\n");
